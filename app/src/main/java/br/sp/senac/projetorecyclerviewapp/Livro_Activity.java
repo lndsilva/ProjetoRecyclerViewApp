9f19f7d739d57ac223a60edf0801103ceeeeba61
lndsilva <lndsilva@hotmail.com>
2019-08-22 16:57:54 -0300

    recyclerView

---
+++ app/src/main/java/br/sp/senac/projetorecyclerviewapp/Livro_Activity.java
@@ -0,0 +1,14 @@
+package br.sp.senac.projetorecyclerviewapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Livro_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.livro_layout);
+    }
+}
